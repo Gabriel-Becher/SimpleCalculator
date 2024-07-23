7f9ba8d137889591c9d18c56bcd6c7b806a8d493
Gabriel <gabriel.abecher@hotmail.com>
2024-07-23 17:00:13 -0300

    "Formatted code"

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,30 @@
-let botoes = document.getElementsByClassName("botao")
+let botoes = document.getElementsByClassName("botao");
 let display = document.getElementById("display");
 
+for (i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener("click", function () {
+    let character = this.value.toString();
 
-for(i=0;i<botoes.length;i++){
-    botoes[i].addEventListener("click", function () {
-        let character = this.value.toString()
-        
-        switch(character){
-            case 'C': display.innerHTML='' 
-            break
-            case '=': display.innerHTML = eval(display.innerHTML)
-            break
-            case 'sqr': display.innerHTML += '*' + display.innerHTML 
-            break
-            case 'root': display.innerHTML = Math.sqrt(parseFloat(display.innerHTML))
-            break
-            default: display.innerHTML += ''+character+''
-        }
-    })
-
-    function calcular(){
-        console.log("executed")
-        return parseFloat(display.innerHTML);
-
+    switch (character) {
+      case "C":
+        display.innerHTML = "";
+        break;
+      case "=":
+        display.innerHTML = eval(display.innerHTML);
+        break;
+      case "sqr":
+        display.innerHTML += "*" + display.innerHTML;
+        break;
+      case "root":
+        display.innerHTML = Math.sqrt(parseFloat(display.innerHTML));
+        break;
+      default:
+        display.innerHTML += "" + character + "";
     }
+  });
+
+  function calcular() {
+    console.log("executed");
+    return parseFloat(display.innerHTML);
+  }
 }
